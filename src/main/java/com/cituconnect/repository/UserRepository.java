c2a5d69ce9a7fa84666ff9e1a8df9e51a9ca18d4
tlsabadinas <treasure.abadinas@gmail.com>
2025-11-12 22:21:52 +0800

    feature(user): add user repository

---
+++ src/main/java/com/cituconnect/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.cituconnect.repository;
+
+import com.cituconnect.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByStudentId(String studentId);
+    List<User> findByIsActiveTrue();
+    Boolean existsByEmail(String email);
+}
