6102e205f0e6dea9ac6cc608c28229259769d97f
tlsabadinas <treasure.abadinas@gmail.com>
2025-11-12 22:23:15 +0800

    feature(like): add like service

---
+++ src/main/java/com/cituconnect/service/LikeService.java
@@ -0,0 +1,92 @@
+package com.cituconnect.service;
+
+import com.cituconnect.entity.Like;
+import com.cituconnect.entity.Forum;
+import com.cituconnect.entity.Discussion;
+import com.cituconnect.entity.User;
+import com.cituconnect.repository.LikeRepository;
+import com.cituconnect.repository.ForumRepository;
+import com.cituconnect.repository.DiscussionRepository;
+import com.cituconnect.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class LikeService {
+    @Autowired
+    private LikeRepository likeRepository;
+
+    @Autowired
+    private ForumRepository forumRepository;
+
+    @Autowired
+    private DiscussionRepository discussionRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Like toggleForumLike(Long userId, Long forumId) {
+        Optional<User> user = userRepository.findById(userId);
+        Optional<Forum> forum = forumRepository.findById(forumId);
+
+        if (user.isEmpty() || forum.isEmpty()) {
+            return null;
+        }
+
+        Optional<Like> existingLike = likeRepository.findByUserUserIdAndForumId(userId, forumId);
+
+        if (existingLike.isPresent()) {
+            // Unlike
+            likeRepository.delete(existingLike.get());
+            return null;
+        } else {
+            // Like
+            Like like = Like.builder()
+                    .user(user.get())
+                    .forum(forum.get())
+                    .build();
+            return likeRepository.save(like);
+        }
+    }
+
+    public Like toggleDiscussionLike(Long userId, Long discussionId) {
+        Optional<User> user = userRepository.findById(userId);
+        Optional<Discussion> discussion = discussionRepository.findById(discussionId);
+
+        if (user.isEmpty() || discussion.isEmpty()) {
+            return null;
+        }
+
+        Optional<Like> existingLike = likeRepository.findByUserUserIdAndDiscussionId(userId, discussionId);
+
+        if (existingLike.isPresent()) {
+            // Unlike
+            likeRepository.delete(existingLike.get());
+            return null;
+        } else {
+            // Like
+            Like like = Like.builder()
+                    .user(user.get())
+                    .discussion(discussion.get())
+                    .build();
+            return likeRepository.save(like);
+        }
+    }
+
+    public boolean isForumLikedByUser(Long userId, Long forumId) {
+        return likeRepository.findByUserUserIdAndForumId(userId, forumId).isPresent();
+    }
+
+    public boolean isDiscussionLikedByUser(Long userId, Long discussionId) {
+        return likeRepository.findByUserUserIdAndDiscussionId(userId, discussionId).isPresent();
+    }
+
+    public Long getForumLikeCount(Long forumId) {
+        return likeRepository.countByForumId(forumId);
+    }
+
+    public Long getDiscussionLikeCount(Long discussionId) {
+        return likeRepository.countByDiscussionId(discussionId);
+    }
+}
