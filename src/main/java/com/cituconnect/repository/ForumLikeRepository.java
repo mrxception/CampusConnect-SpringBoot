3f01d1b314820a3f2dcb588157c03d6deb2ed9a9
tlsabadinas <treasure.abadinas@gmail.com>
2025-12-14 21:49:49 +0800

    feature(forumlike): add forum like repository

---
+++ src/main/java/com/cituconnect/repository/ForumLikeRepository.java
@@ -0,0 +1,15 @@
+package com.cituconnect.repository;
+
+import com.cituconnect.entity.ForumLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface ForumLikeRepository extends JpaRepository<ForumLike, Long> {
+    Optional<ForumLike> findByUserUserIdAndForumId(Long userId, Long forumId);
+    Long countByForumId(Long forumId);
+
+    void deleteByForumId(Long forumId);
+    void deleteByUserUserId(Long userId);
+}
